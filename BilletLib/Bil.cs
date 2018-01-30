1b5de294c00e43b7fe6953fe5f843434ef784c46
ProfessorGenki <getyowalkon@gmail.com>
2018-01-30 14:40:36 +0100

    initial commit + bil class

---
+++ BilletLib/Bil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BilletLib
+{
+    public class Bil
+    {
+        public string _nummerplade;
+        public DateTime _dato;
+    }
+}
--- BilletLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BilletLib
-{
-    public class Class1
-    {
-    }
-}
